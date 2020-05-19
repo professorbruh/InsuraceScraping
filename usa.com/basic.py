50408771e2600462769e7f5639b9b2ebc0c37045
professorbruh <62765220+professorbruh@users.noreply.github.com>
2020-05-20 02:11:18 +0530

    Dictionaries Done

--- usa.com/basic.py
+++ usa.com/basic.py
@@ -4,3 +4,42 @@ from urllib.request import urlopen as uReq
 
 import json
 
+url = "http://www.usa.com/02111-ma.htm"
+
+uClient = uReq(url)
+
+page_html=uClient.read()
+
+uClient.close()
+
+parsed_page = soup(page_html, "html.parser")
+
+container = parsed_page.find("td",valign ="top")
+
+tables = container.find_all("tr")
+
+dict_obj = {}
+
+for i in tables:
+    table_container = i.find_all("td")
+
+    if table_container[1].text.find("rank #") == -1:
+        dict_obj[table_container[0].text] = table_container[1].text
+
+    elif table_container[1].text.find("see\x") > 0:
+        dict_obj[table_container[0].text] = table_container[1].text
+
+    else:
+        print("yes")
+
+
+print(dict_obj)
+
+
+
+
+
+
+
+
+
